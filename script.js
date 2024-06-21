3b4b84b305aea28a67392494351bc254b14f2540
Ana Santos <ana.br@hotmail.com>
2024-06-21 15:38:54 -0300

    colocação dos icones indicativos da pagina

--- script.js
+++ script.js
@@ -56,3 +56,40 @@ function limparFormulario() {
     });
     
     
+    document.addEventListener("DOMContentLoaded", function () {
+      const form = document.getElementById("contactForm2");
+      const formResponse2 = document.getElementById("formResponse2");
+    
+      form.addEventListener("submit", async function (event) {
+        event.preventDefault();
+    
+        const formData = new FormData(form);
+        const plainFormData = Object.fromEntries(formData.entries());
+        const formDataJsonString = JSON.stringify(plainFormData);
+    
+        try {
+          const response = await fetch("https://formspree.io/f/mwpeevyg" ,{
+            method: "POST",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json"
+            },
+            body: formDataJsonString
+          });
+    
+          if (response.ok) {
+            formResponse2.innerHTML = "<p>E-mail enviado com sucesso!</p>";
+            form.reset();
+          } else {
+            formResponse2.innerHTML = "<p>Ocorreu um erro ao enviar o e-mail. Tente novamente mais tarde.</p>";
+          }
+        } catch (error) {
+          formResponse2.innerHTML = "<p>Ocorreu um erro ao enviar o e-mail. Tente novamente mais tarde.</p>";
+        }
+    
+        // Clear message after 20 seconds
+        setTimeout(() => {
+          formResponse2.innerHTML = "";
+        }, 10000);
+      });
+    });
