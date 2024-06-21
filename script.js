99a2404339e44814f4bee0d3edca3046a9bbb90e
Ana Santos <ana.br@hotmail.com>
2024-06-21 13:26:39 -0300

    Integração do formulário de contato com o formspree

--- script.js
+++ script.js
@@ -14,3 +14,45 @@ function limparFormulario() {
 
     // Insere o ano atual no elemento <span>
     yearSpan.textContent = currentYear;
+
+
+    //formulário de contato
+    document.addEventListener("DOMContentLoaded", function () {
+      const form = document.getElementById("contactForm");
+      const formResponse = document.getElementById("formResponse");
+    
+      form.addEventListener("submit", async function (event) {
+        event.preventDefault();
+    
+        const formData = new FormData(form);
+        const plainFormData = Object.fromEntries(formData.entries());
+        const formDataJsonString = JSON.stringify(plainFormData);
+    
+        try {
+          const response = await fetch("https://formspree.io/f/manwwqpb", {
+            method: "POST",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json"
+            },
+            body: formDataJsonString
+          });
+    
+          if (response.ok) {
+            formResponse.innerHTML = "<p>E-mail enviado com sucesso!</p>";
+            form.reset();
+          } else {
+            formResponse.innerHTML = "<p>Ocorreu um erro ao enviar o e-mail. Tente novamente mais tarde.</p>";
+          }
+        } catch (error) {
+          formResponse.innerHTML = "<p>Ocorreu um erro ao enviar o e-mail. Tente novamente mais tarde.</p>";
+        }
+    
+        // Clear message after 20 seconds
+        setTimeout(() => {
+          formResponse.innerHTML = "";
+        }, 10000);
+      });
+    });
+    
+    
